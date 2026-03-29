6a387526e051b45f8bcbbd641c85cbdbd163274e
RidhiGupta001 <142829845+RidhiGupta001@users.noreply.github.com>
2026-03-29 22:58:04 +0530

    Leetcode53

---
+++ leetcode53.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+//using kadane's algorithm
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+    int n = nums.size();
+    int sum = 0;
+    int maxsum = INT_MIN;
+    for(int i = 0;i<n;i++){
+    sum+=nums[i];
+    maxsum = max(maxsum,sum);
+    if(sum<0){
+        sum = 0;
+    }
+    }
+    return maxsum;   
+    }
+};
